0174bc903b5679c808a76ddd9d3546e8524c503a
norman-c <52259325+norman-c@users.noreply.github.com>
2020-12-03 06:02:53 -0800

    help

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "123456",
-    DB: "testdb"
+    HOST: "us-cdbr-east-02.cleardb.com",
+    USER: "bb9d2d5cab8605",
+    PASSWORD: "d10e9c3e",
+    DB: "heroku_ba7834bb2a8f15d"
 };
