04dbeb9ee7d0363c283f13fe1883e56c9b6ddf7f
norman-c <52259325+norman-c@users.noreply.github.com>
2020-12-04 05:27:14 -0800

    no upload pic yet

--- app/routes/owner.routes.js
+++ app/routes/owner.routes.js
@@ -2,25 +2,25 @@ module.exports = app => {
     const owners = require("../controllers/owner.controller.js");
 
     // Create a new Owner
-    // https://petsapi4537.herokuapp.com/owners?name=norman&age=11&petid=61
-    app.post("/owners", owners.create);
+    // https://petsapi4537.herokuapp.com/api/v1/owners?name=norman&age=11&petid=61
+    app.post("/api/v1/owners", owners.create);
 
 
     // Retrieve all Owners
-    // https://petsapi4537.herokuapp.com/owners
-    app.get("/owners", owners.findAll);
+    // https://petsapi4537.herokuapp.com/api/v1/owners
+    app.get("/api/v1/owners", owners.findAll);
 
     // Retrieve a single owner with ownerId
-    // https://petsapi4537.herokuapp.com/owners?name=norman&age=11&petid=11
-    app.get("/owners/ownerId", owners.findOne);
+    // https://petsapi4537.herokuapp.com/api/v1/owners?name=norman&age=11&petid=11
+    app.get("/api/v1/owners/ownerId", owners.findOne);
 
     // Update a Owner with petId
     //- POST request, needs to be sent in the fetch by client will be a JSON
-    app.put("/owners/update", owners.update);
+    app.put("/api/v1/owners/update", owners.update);
 
     // Delete a Owner with petId
-    // https://petsapi4537.herokuapp.com/owners/delete?id=11
-    app.delete("/owners/delete", owners.delete);
+    // https://petsapi4537.herokuapp.com/api/v1/owners/delete?id=11
+    app.delete("/api/v1/owners/delete", owners.delete);
 
 
 };
--- app/routes/pet.routes.js
+++ app/routes/pet.routes.js
@@ -2,40 +2,40 @@ module.exports = app => {
     const pets = require("../controllers/pet.controller.js");
 
     // Create a new Pet
-    //https://petsapi4537.herokuapp.com/pets/?name=lg&type=cat&breed=white cat&age=6
-    app.post("/pets", pets.create);
+    //https://petsapi4537.herokuapp.com/api/v1/pets/?name=lg&type=cat&breed=white cat&age=6
+    app.post("/api/v1/pets", pets.create);
 
-    // app.post("/pets/upload", upload.single("file"), uploadController.uploadFiles);
+    // app.post("/api/v1/pets/upload", upload.single("file"), uploadController.uploadFiles);
 
 
     // Retrieve all Pets
-    // https://petsapi4537.herokuapp.com/pets/
-    app.get("/pets", pets.findAll);
+    // https://petsapi4537.herokuapp.com/api/v1/pets/
+    app.get("/api/v1/pets", pets.findAll);
 
     // Retrieve all Pets by type
-    // To call https://petsapi4537.herokuapp.com/pets/pettype?type=dog
-    app.get("/pets/pettype", pets.findAllByType);
+    // To call https://petsapi4537.herokuapp.com/api/v1/pets/pettype?type=dog
+    app.get("/api/v1/pets/pettype", pets.findAllByType);
 
     // Retrieve a single Pet with petId
-    // https://petsapi4537.herokuapp.com/pets/petid?petid=11
-    app.get("/pets/petid", pets.findOne);
+    // https://petsapi4537.herokuapp.com/api/v1/pets/petid?petid=11
+    app.get("/api/v1/pets/petid", pets.findOne);
 
 
 
     // Update a Pet with petId
     //- POST request, needs to be sent in the fetch by client will be a JSON
-    app.put("/pets/update", pets.update);
+    app.put("/api/v1/pets/update", pets.update);
 
 
 
 
     // Delete a Pet with petId
-    // https://petsapi4537.herokuapp.com/pets/delete?petid=51
-    app.delete("/pets/delete", pets.delete);
+    // https://petsapi4537.herokuapp.com/api/v1/pets/delete?petid=51
+    app.delete("/api/v1/pets/delete", pets.delete);
 
     // Delete all Pets by type
-    //https://petsapi4537.herokuapp.com/pets/detelebytype?type=cat
-    app.delete("/pets/detelebytype", pets.deleteAllByType);
+    //https://petsapi4537.herokuapp.com/api/v1/pets/detelebytype?type=cat
+    app.delete("/api/v1/pets/detelebytype", pets.deleteAllByType);
 
 
 };
