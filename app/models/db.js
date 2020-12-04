b7f3eb50a7a25ab9ea9c29a68e77c88a66664df0
norman-c <52259325+norman-c@users.noreply.github.com>
2020-12-04 04:25:09 -0800

    no upload pic yet

--- app/models/db.js
+++ app/models/db.js
@@ -2,14 +2,21 @@ const mysql = require("mysql");
 const dbConfig = require("../config/db.config.js");
 
 // Create a connection to the database
-const connection = mysql.createPool({
-    connectionLimit: 10,
+const connection = mysql.createConnection({
+
     host: dbConfig.HOST,
     user: dbConfig.USER,
     password: dbConfig.PASSWORD,
     database: dbConfig.DB
 });
 
+// let pool = mysql.createPool({
+//     connectionLimit: 10,
+//     host: "us-cdbr-east-02.cleardb.com",
+//     user: "b64404cf689cb3",
+//     password: "c57933b1",
+//     database: "heroku_248f2bc82734799"
+// });
 
 // open the MySQL connection
 connection.connect(error => {
