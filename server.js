48f972f325679fb60f22370cac753c51c3d12e5c
norman-c <52259325+norman-c@users.noreply.github.com>
2020-12-03 19:34:21 -0800

    hello

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 });
 
 
-require("./app/routes/customer.routes.js")(app);
+require("./app/routes/pet.routes.js")(app);
 // set port, listen for requests
 app.listen(3000, () => {
     console.log("Server is running on port 3000.");
